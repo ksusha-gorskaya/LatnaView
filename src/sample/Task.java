fa204b537a7f58cbb885fa8989ac6e8996ec7334
ksusha-gorskaya <ksusha-gorskaya@mail.ru>
2017-02-04 16:25:56 +0300

    Added class Storekeeper, class Task
    Read the information in the warehouse (cell) by column; Started read
    test_log

---
+++ src/sample/Task.java
@@ -0,0 +1,51 @@
+package sample;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Ксения Горская on 04.02.2017.
+ */
+public class Task {
+    private Integer id;
+    private String time;
+    private ArrayList<String[]> listTask;
+
+    public Task() {
+        super();
+        listTask = new ArrayList<>();
+        id=0;
+        time="";
+    }
+
+    public void clearAll() {
+        try {
+            listTask.clear();
+        } catch (NullPointerException npe) {
+
+        }
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public ArrayList<String[]> getListTask() {
+        return listTask;
+    }
+
+    public void setListTask(ArrayList<String[]> listTask) {
+        this.listTask = listTask;
+    }
+}
